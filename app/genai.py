73c0a1e5b758581df1aa88a4ad4c125b345aef27
Nilesh Popli <nileshpopli23@gmail.com>
2023-08-19 20:54:40 +0530

    added converstion logic

--- app/genai.py
+++ app/genai.py
@@ -2,10 +2,76 @@ from PIL import Image
 import io
 import os
 import requests
+import os
+import openai
+import requests
+import json
+from IPython.display import Image, display
+from IPython.display import HTML
 from .azure_setup import upload_to_blob_storage
 
 API_URL = "https://api-inference.huggingface.co/models/Adrenex/fastgen"
 headers = {"Authorization": "Bearer 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"}
+openai.api_key  = "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"
+
+context = [ {'role':'system', 'content':"""
+You are a NLP expert and do the following:
+1) User will enter a prompt (to a text to image model), then the prompt is to be returned.
+2) Then again, the user will be asked to enter another prompt, which will be in continuation of the first prompt. Then this needs to be merged appropriately with the first prompt, and new prompt will be returned.
+3) This will go on and on as long as user inputs.
+4) Make sure to Strictly return the new prompt only, nothing else, even without The first prompt is.
+
+Example 1: 
+First prompt: Red kurta
+Return: Red Kurta
+
+Second Prompt: Keep it long
+Return: Red long kurta
+
+Third Prompt: suitable for diwali occasion
+Return: Red long kurta for Diwali
+
+Forth Prompt: For kids
+Display: Red long kurta for Kids and for Diwali
+
+Return: Keep it short
+Display: Red short kurta for Kids and for Diwali
+
+Notice that, Short will replace Long. 
+
+Example 2:
+First prompt: Green Tshirt
+Return: Green Tshirt
+
+Second Prompt: make it red
+Return: Red Tshirt
+
+Notice that red replaces Green since both are colors
+"""}]
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def collect_messages(prompt):
+
+
+    #Yaha pe context fetch krna hai db se
+
+
+    context.append({'role':'user', 'content':f"{prompt}"})
+    response = get_completion_from_messages(context) 
+    context.append({'role':'assistant', 'content':f"{response}"})
+
+
+    #yaha pe content push krna hai db pe
+
+
+    return response
 
 
 def query(payload):
@@ -14,8 +80,13 @@ def query(payload):
 
 
 def text2image(prompt):
+
+    new_prompt = collect_messages(prompt)
+
+    #ye new prompt print krvana hai web page pe
+
     image_bytes = query({
-        "inputs": prompt,
+        "inputs": new_prompt,
     })
 
     image = Image.open(io.BytesIO(image_bytes))
